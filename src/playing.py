e46a8478f99105718298c4814c7d62772dc31d83
BrentWeiffenbach <rweiff2022@gmail.com>
2025-04-17 00:43:30 -0400

    Reorganized, deleted unncessary things, updated gitignore. Track creation and custom tracks should work fine. MAJOR BUG: Seems like training freezes after a certain point, i think we will need to rework the trainign script

---
+++ src/playing.py
@@ -0,0 +1,115 @@
+"""
+Once a model is learned, use this to play it. that is run/exploit a policy to get the feature expectations of the policy
+"""
+
+import argparse
+import os
+
+import numpy as np
+
+from flat_game import carmunk
+from nn import neural_net
+
+NUM_STATES = 8
+GAMMA = 0.9
+
+
+def play(model, weights, track_file="tracks/default.json"):
+    car_distance = 0
+    game_state = carmunk.GameState(weights, track_file)
+
+    _, state, __ = game_state.frame_step((2))
+
+    featureExpectations = np.zeros(len(weights))
+
+    # Move.
+    # time.sleep(15)
+    while True:
+        car_distance += 1
+
+        # Choose action.
+        action = np.argmax(model.predict(state, batch_size=1))
+        # print ("Action ", action)
+
+        # Take action.
+        immediateReward, state, readings = game_state.frame_step(action)
+        # print ("immeditate reward:: ", immediateReward)
+        # print ("readings :: ", readings)
+        # start recording feature expectations only after 100 frames
+        if car_distance > 100:
+            featureExpectations += (GAMMA ** (car_distance - 101)) * np.array(readings)
+        # print ("Feature Expectations :: ", featureExpectations)
+        # Tell us something.
+        if car_distance % 2000 == 0:
+            print("Current distance: %d frames." % car_distance)
+            break
+
+    return featureExpectations
+
+
+if __name__ == "__main__":
+    # added default args for red model
+    parser = argparse.ArgumentParser(
+        description="Run a trained model with a specific track"
+    )
+    parser.add_argument("behavior", nargs="?", default="red", help="Behavior name")
+    parser.add_argument("iteration", nargs="?", default="8", help="Iteration number")
+    parser.add_argument("frame", nargs="?", default="100000", help="Frame number")
+    parser.add_argument(
+        "--track",
+        "-t",
+        type=str,
+        default="tracks/default.json",
+        help="Path to obstacle configuration file (default: tracks/default.json)",
+    )
+    args = parser.parse_args()
+
+    BEHAVIOR = args.behavior
+    ITERATION = args.iteration
+    FRAME = args.frame
+
+    # Resolve the track file path if provided
+    track_file = "tracks/default.json"  # Set a default value
+    if args.track:
+        # Try different path combinations
+        potential_paths = [
+            args.track,  # Direct path
+            os.path.join("tracks", args.track),  # In tracks folder
+            os.path.join(
+                "tracks", f"{args.track}.json"
+            ),  # In tracks folder with .json extension
+        ]
+
+        for path in potential_paths:
+            if os.path.exists(path):
+                track_file = path
+                print(f"Using track file: {path}")
+                break
+
+            else:
+                print(f"Warning: Could not find track file at {args.track}")
+                print(f"Tried: {potential_paths}")
+                print("Using default track instead.")
+
+    saved_model = (
+        "saved-models/"
+        + "saved-models_"
+        + BEHAVIOR
+        + "/evaluatedPolicies/"
+        + str(ITERATION)
+        + "-164-150-100-50000-"
+        + str(FRAME)
+        + ".h5"
+    )
+    weights = [
+        -0.79380502,
+        0.00704546,
+        0.50866139,
+        0.29466834,
+        -0.07636144,
+        0.09153848,
+        -0.02632325,
+        -0.09672041,
+    ]
+    model = neural_net(NUM_STATES, [164, 150], saved_model)
+    print(play(model, weights, track_file))
---
+++ src/toy_car_IRL.py
@@ -0,0 +1,187 @@
+# IRL algorith developed for the toy car obstacle avoidance problem for testing.
+import logging
+
+import numpy as np
+from cvxopt import (
+    matrix,
+    solvers,  # convex optimization library
+)
+
+from learning import IRL_helper  # get the Reinforcement learner
+from nn import neural_net  # construct the nn and send to playing
+from playing import (
+    play,
+)  # get the RL Test agent, gives out feature expectations after 2000 frames
+
+NUM_STATES = 8
+BEHAVIOR = "red"  # yellow/brown/red/bumping
+FRAMES = 100000  # number of RL training frames per iteration of IRL
+
+
+class irlAgent:
+    def __init__(self, randomFE, expertFE, epsilon, num_states, num_frames, behavior):
+        self.randomPolicy = randomFE
+        self.expertPolicy = expertFE
+        self.num_states = num_states
+        self.num_frames = num_frames
+        self.behavior = behavior
+        self.epsilon = epsilon  # termination when t<0.1
+        self.randomT = np.linalg.norm(
+            np.asarray(self.expertPolicy) - np.asarray(self.randomPolicy)
+        )  # norm of the diff in expert and random
+        self.policiesFE = {
+            self.randomT: self.randomPolicy
+        }  # storing the policies and their respective t values in a dictionary
+        print("Expert - Random at the Start (t) :: ", self.randomT)
+        self.currentT = self.randomT
+        self.minimumT = self.randomT
+
+    def getRLAgentFE(
+        self, W, i
+    ):  # get the feature expectations of a new poliicy using RL agent
+        IRL_helper(
+            W, self.behavior, self.num_frames, i
+        )  # train the agent and save the model in a file used below
+        saved_model = (
+            "saved-models/"
+            + "saved-models_"
+            + self.behavior
+            + "/evaluatedPolicies/"
+            + str(i)
+            + "-164-150-100-50000-"
+            + str(self.num_frames)
+            + ".h5"
+        )  # use the saved model to get the FE
+        model = neural_net(self.num_states, [164, 150], saved_model)
+        return play(
+            model, W
+        )  # return feature expectations by executing the learned policy
+
+    def policyListUpdater(self, W, i):  # add the policyFE list and differences
+        tempFE = self.getRLAgentFE(
+            W, i
+        )  # get feature expectations of a new policy respective to the input weights
+        hyperDistance = np.abs(
+            np.dot(W, np.asarray(self.expertPolicy) - np.asarray(tempFE))
+        )  # hyperdistance = t
+        self.policiesFE[hyperDistance] = tempFE
+        return hyperDistance  # t = (weights.tanspose)*(expert-newPolicy)
+
+    def optimalWeightFinder(self):
+        f = open("weights-" + BEHAVIOR + ".txt", "w")
+        i = 1
+        while True:
+            W = (
+                self.optimization()
+            )  # optimize to find new weights in the list of policies
+            print("weights ::", W)
+            f.write(str(W))
+            f.write("\n")
+            print("the distances  ::", self.policiesFE.keys())
+            self.currentT = self.policyListUpdater(W, i)
+            print("Current distance (t) is:: ", self.currentT)
+            if (
+                self.currentT <= self.epsilon
+            ):  # terminate if the point reached close enough
+                break
+            i += 1
+        f.close()
+        return W
+
+    def optimization(
+        self,
+    ):  # implement the convex optimization, posed as an SVM problem
+        m = len(self.expertPolicy)
+        P = matrix(2.0 * np.eye(m), tc="d")  # min ||w||
+        q = matrix(np.zeros(m), tc="d")
+        policyList = [self.expertPolicy]
+        h_list = [1]
+        for i in self.policiesFE.keys():
+            policyList.append(self.policiesFE[i])
+            h_list.append(1)
+        policyMat = np.matrix(policyList)
+        policyMat[0] = -1 * policyMat[0]
+        G = matrix(policyMat, tc="d")
+        h = matrix(-np.array(h_list), tc="d")
+        sol = solvers.qp(P, q, G, h)
+
+        weights = np.squeeze(np.asarray(sol["x"]))
+        norm = np.linalg.norm(weights)
+        weights = weights / norm
+        return weights  # return the normalized weights
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    randomPolicyFE = [
+        7.74363107,
+        4.83296402,
+        6.1289194,
+        0.39292849,
+        2.0488831,
+        0.65611318,
+        6.90207523,
+        2.46475348,
+    ]
+    # ^the random policy feature expectations
+    expertPolicyYellowFE = [
+        7.5366e00,
+        4.6350e00,
+        7.4421e00,
+        3.1817e-01,
+        8.3398e00,
+        1.3710e-08,
+        1.3419e00,
+        0.0000e00,
+    ]
+    # ^feature expectations for the "follow Yellow obstacles" behavior
+    expertPolicyRedFE = [
+        7.9100e00,
+        5.3745e-01,
+        5.2363e00,
+        2.8652e00,
+        3.3120e00,
+        3.6478e-06,
+        3.82276074e00,
+        1.0219e-17,
+    ]
+    # ^feature expectations for the follow Red obstacles behavior
+    expertPolicyBrownFE = [
+        5.2210e00,
+        5.6980e00,
+        7.7984e00,
+        4.8440e-01,
+        2.0885e-04,
+        9.2215e00,
+        2.9386e-01,
+        4.8498e-17,
+    ]
+    # ^feature expectations for the "follow Brown obstacles" behavior
+    expertPolicyBumpingFE = [
+        7.5313e00,
+        8.2716e00,
+        8.0021e00,
+        2.5849e-03,
+        2.4300e01,
+        9.5962e01,
+        1.5814e01,
+        1.5538e03,
+    ]
+    # ^feature expectations for the "nasty bumping" behavior
+    expertPolicyFindRed = [
+        5.39704807,
+        3.46564062,
+        3.32507319,
+        0.0,
+        9.99757251,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    epsilon = 0.1
+    irlearner = irlAgent(
+        randomPolicyFE, expertPolicyFindRed, epsilon, NUM_STATES, FRAMES, BEHAVIOR
+    )
+    print(irlearner.optimalWeightFinder())
