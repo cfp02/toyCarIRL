413b9d24b0ba1e284ffc23135ceb410473277939
Cole Parks <coleparks13@gmail.com>
2025-04-17 22:02:26 -0400

    added mps for mac gpu training

--- src/nn.py
+++ src/nn.py
@@ -11,7 +11,7 @@ import torch.optim as optim
 # Import your car environment
 
 # Set up device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {device}")
 
 
